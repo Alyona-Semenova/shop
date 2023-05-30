b8aa60841ef6bef5a5750a638a1825f26ef03782
Alyona Semyonova <invisible2234@gmail.com>
2023-05-30 20:12:55 +0700

    main page

---
+++ script.js
@@ -0,0 +1,41 @@
+let cardContainer = document.querySelector(".product-cards");
+
+let cardHeader = "ELLERY X M'O CAPSULE";
+let cardDescription =
+    "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.";
+let price = "$52.00";
+
+// for (let i = 1; i <= i++; ) {
+//     cardContainer.innerHTML = `<div class="product-cards__item card-item">  
+//     <div class="card-item__img"> </div> 
+//     <div class="card-item__header"> 
+//     ${cardHeader}
+//     </div> 
+//     <div class="card-item__description"> 
+//     ${cardDescription}
+//     </div> 
+//     <div class="card-item__price"> 
+//     ${price}
+//     </div>  
+// </div> `
+// console.log(i);
+// }
+
+
+
+// cardContainer.innerHTML = Array(6)
+//     .fill(
+//         `<div class="product-cards__item card-item">  
+//             <div class="card-item__img"> </div> 
+//             <div class="card-item__header"> 
+//             ${cardHeader}
+//             </div> 
+//             <div class="card-item__description"> 
+//             ${cardDescription}
+//             </div> 
+//             <div class="card-item__price"> 
+//             ${price}
+//             </div>  
+//         </div>`
+//     )
+//     .join("");
